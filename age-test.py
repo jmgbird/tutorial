4c29d0ea34e55323d5c55a18e2b968c22190c371
James Gilberti <james.gilberti@nielsen.com>
2018-03-23 10:05:49 -0400

    Added an age test file

---
+++ age-test.py
@@ -0,0 +1,13 @@
+#install package
+a = int(input("How old are you , "))
+print(a)
+if a in range(1, 20):
+	print('You are too young, move on')
+elif a in range(21, 60):
+	print('You are able to drink')
+elif a in range(61, 100):
+	print('You are too old, go home')
+else:
+	print('You aren''t born')
+
+
